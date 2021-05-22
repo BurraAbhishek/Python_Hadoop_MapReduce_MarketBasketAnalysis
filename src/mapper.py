1b12db4452f87db6b4a40427a05aa3a90830a609
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-05-22 09:57:56 +0530

    Add mapper code with a test case

---
+++ src/mapper.py
@@ -0,0 +1,36 @@
+import sys
+import itertools
+
+
+def mapper(n):
+    """ Emits k itemsets of the form <item, count> from a dataset """
+
+    # Step 1: Generate the dataset
+    finaldataset = []
+    for line in sys.stdin:
+        line = line.strip()
+        words = line.split(",")
+        datasubset = []
+        uniquedata = []
+        for word in words:
+            # Step 2: Map duplicates
+            if word in uniquedata:
+                for i in range(0, len(datasubset)):
+                    if word == datasubset[i][0]:
+                        datasubset[i][1] += 1
+            else:
+                datasubset.append([word, 1])
+                uniquedata.append(word)
+        # Step 3: Sort the dataset to avoid duplicated keys
+        datasubset.sort()
+        # Step 4: Combine the items within a single transaction
+        if len(datasubset) > 0:
+            finaldatasubset = list(itertools.combinations(datasubset, n))
+            if len(finaldatasubset) > 0:
+                finaldataset.append(finaldatasubset)
+    # Dataset is created as an input to reducer
+        print(datasubset)
+
+# Test case
+mapper(2)
+
