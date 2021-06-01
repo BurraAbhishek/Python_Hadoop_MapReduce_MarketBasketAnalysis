e9f4e5e716c42b46d7662aad9069835195cf014a
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-06-01 23:38:45 +0530

    Create reset.py
    After completing the program, the user might want to run the program on another dataset. This module resets all the changed settings and resets the list of discarded items.

---
+++ src/reset.py
@@ -0,0 +1,20 @@
+import json
+
+# Reset all settings
+try:
+    # Load the JSON settings file
+    with open("apriori_settings.json", "r") as j:
+        configs = json.load(j)
+    # Reset the pass attribute to 1
+    configs["itemset-length"] = 1
+    # Save the updated settings
+    f = open("apriori_settings.json", "w")
+    json.dump(configs, f, indent=4)
+    f.close()
+    # Reset the list of discarded items to an empty list.
+    o = open("part-00000", "w")
+    o.write("[]")
+    o.close()
+except:
+    # Print error message
+    print("Couldn't reset the program.")
