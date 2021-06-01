b1ec9d0bac4fa718f09f8b30a968126eb5344e90
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-06-01 23:28:41 +0530

    Create nextpass.py

---
+++ src/nextpass.py
@@ -0,0 +1,18 @@
+import json
+
+# Indicate next pass
+# Add 1 to the pass attribute in the JSON file
+# This module can run without Hadoop.
+try:
+    # Load the JSON settings file
+    with open("apriori_settings.json", "r") as j:
+        configs = json.load(j)
+    # Increment the pass attribute by 1
+    configs["itemset-length"] += 1
+    # Save the updated settings
+    f = open("apriori_settings.json", "w")
+    json.dump(configs, f, indent=4)
+    f.close()
+except:
+    # Do something else
+    f = 0
